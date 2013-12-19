3a371cc4ac6473f3f0efb9bf30ad597b3d8a5d31
mfdii <michael@ducy.org>
2013-12-18 21:46:06 -0500

    fix method call

--- cookbooks/constore/providers/cookbook.rb
+++ cookbooks/constore/providers/cookbook.rb
@@ -16,8 +16,8 @@ action :upload do
 	converge_by("Uploading Cookbooks in #{new_resource.cookbook_path}.") do
 
 		rest = Chef::REST.new(new_resource.url,new_resource.client_name,new_resource.client_key)
-
-		cu = Chef::CookbookUploader.new(Chef::CookbookLoader.load_cookbooks(new_resource.cookbook_path), new_resource.cookbook_path,{"rest" => rest})
+		cl = Chef::CookbookLoader.new(new_resource.cookbook_path)
+		cu = Chef::CookbookUploader.new(cl.load_cookbooks, new_resource.cookbook_path,{"rest" => rest})
 
 		cu.upload_cookbooks
 
