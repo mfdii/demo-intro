83ef68adec8c022a594ecf8cedba4b104848402a
mfdii <michael@ducy.org>
2013-12-18 22:05:42 -0500

    resort to execute because I suck

--- cookbooks/constore/providers/cookbook.rb
+++ cookbooks/constore/providers/cookbook.rb
@@ -13,7 +13,7 @@ end
 
 action :upload do
 
-	converge_by("Uploading Cookbooks in #{new_resource.cookbook_path}.") do
+	converge_by("Uploading Cookbooks in #{@new_resource.cookbook_path}.") do
 
 		#rest = Chef::REST.new(new_resource.url,new_resource.client_name,new_resource.client_key)
 		#cl = Chef::CookbookLoader.new(new_resource.cookbook_path)
@@ -22,7 +22,7 @@ action :upload do
 		#cu.upload_cookbooks
 
 		execute "knife cookbook upload" do
-			command "knife cookbook upload -a -s #{new_resource.url} -o #{new_resource.cookbook_path} -k #{new_resource.client_key}"
+			command "knife cookbook upload -a -s #{@new_resource.url} -o #{@new_resource.cookbook_path} -k #{@new_resource.client_key}"
 			action :run
 		end
 		
